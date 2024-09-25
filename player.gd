88a3fb2fcbb1d94d64ac5dab06840bf7d4649142
jackson <jacksonedds04@gmail.com>
2024-09-24 20:35:48 -0500

    Improved character movement/animations

--- player.gd
+++ player.gd
@@ -7,36 +7,37 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	$AnimatedSprite2D.animation = "falling"
+	$AnimatedSprite2D.animation = "idle"
+	$AnimatedSprite2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO
-	$AnimatedSprite2D.play()
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-		$AnimatedSprite2D.animation = "walk_right"
-		# if on cloud
-			# $AnimatedSprite2D.animation = "walk_right"
-		# else
-			# $AnimatedSprite2D.animation = "falling"
-	else:
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 2
+			
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-		$AnimatedSprite2D.animation = "idle"
+		if Input.is_action_pressed("move_right"):
+			velocity.x += 2
+		
+		
+	if Input.is_action_just_released("move_right"):
+		velocity.x -= 1
 		# if on cloud
 			# $AnimatedSprite2D.animation = "idle"
 		# else
 			# $AnimatedSprite2D.animation = "falling"
-		
 	
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-		# if on cloud
-			# $AnimatedSprite2D.animation = "walk_left"
-		# else
-			# $AnimatedSprite2D.animation = "falling"
+	
+	#if Input.is_action_pressed("move_left"):
+		#velocity.x -= 1
+		#if Input.is_action_pressed("move_right"):
+			#velocity.x += 1
 		
 	if Input.is_action_just_released("move_left"):
 		velocity.x += 1
@@ -45,12 +46,28 @@ func _process(delta):
 		# else
 			# $AnimatedSprite2D.animation = "falling"
 		
-		
-	# if up is pressed
+	#if up is pressed
 		#if player is on cloud
-			#velocity.y -= 1
-			#set animation to falling
+		#velocity.y -= 1
+		#set animation to falling
 			
+			
+	if velocity.x == -1:
+		# if on cloud
+		$AnimatedSprite2D.animation = "walk_left"
+	if velocity.x == 0:
+		# if on cloud
+		$AnimatedSprite2D.animation = "idle"
+		# else
+			# $AnimatedSprite2D.animation = "falling"
+	if velocity.x == 1:
+		#if on cloud
+		$AnimatedSprite2D.animation = "walk_right"
+		
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
